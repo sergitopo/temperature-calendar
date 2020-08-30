cdf6bb905c53ee988e1e8388074f8bff82eda915
sergitopo <sergitopodeseca@gmail.com>
2020-08-30 21:27:22 +0200

    modal component on click day, fetch to elastic server working

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,25 +1,40 @@
 <template>
     <div id="app">
-        <calendar ref="calendar" v-if="finishProcess" :attributes="dates" is-expanded :columns="$screens({ lg: 4, md: 2 }, 1)" :rows="$screens({ lg: 3, md: 6 }, 12)"></calendar>
+        <calendar @dayclick="getDayTemperatures" :from-date="new Date('01-01-2020')" :to-date="new Date()" ref="calendar" v-if="finishProcess" :attributes="dates" is-expanded :columns="$screens({ lg: 4, md: 2 }, 1)" :rows="$screens({ lg: 3, md: 6 }, 12)"></calendar>
+        <modal v-if="showDateInfo" @close="showDateInfo = false">
+            <slot name="header">
+                <h3 slot="header">{{daySelected.day}}</h3>
+            </slot>
+            <slot name="body">
+                <div slot="body">
+                    <div style="padding-bottom: 1em">{{`Temperatura mitja del dia: ${daySelected.dayTemperature}`}}</div>
+                    <div>{{`Temperatura mitja habitual de: ${Math.round(daySelected.normalDayTemperature * 100) / 100}`}}</div>
+                </div>
+            </slot>
+        </modal>
     </div>
 </template>
 
 <script>
 
-import weekOfYearAvg from './weekOfYearAvgResponse.json';
-import dailyTemperature from './response.json';
+import modal from './components/Modal';
 
 export default {
     name: 'App',
+    components: {
+         modal
+    },
     data() {
         return {
+            daySelected: null,
             dailyTemperature: null,
+            showDateInfo: false,
             finishProcess: false,
             dates: [
                 {
                     key: 'blue',
                     popover: {
-                        label: `A`
+                        label: `Temperatura mitja >1.5º per baix del normal`
                     },
                     highlight: {
                         contentClass: 'anomaly-cold--1',
@@ -30,7 +45,7 @@ export default {
                  {
                     key: 'indigo',
                     popover: {
-                        label: `A`
+                        label: `Temperatura mitja >3.5º per baix del normal`
                     },
                     highlight: {
                         contentClass: 'anomaly-cold--2',
@@ -41,7 +56,7 @@ export default {
                  {
                     key: 'purple',
                     popover: {
-                        label: `A`
+                        label: `Temperatura mitja >5º per baix del normal`
                     },
                     highlight: {
                         contentClass: 'anomaly-cold--3',
@@ -51,6 +66,9 @@ export default {
                 },
                  {
                     key: 'red',
+                    popover: {
+                        label: `Temperatura mitja >5º per dalt del normal`
+                    },
                     highlight: {
                         contentClass: 'anomaly-hot--3',
                         fillMode: 'light'
@@ -60,7 +78,7 @@ export default {
                  {
                     key: 'orange',
                     popover: {
-                        label: `A`
+                        label: `Temperatura mitja >3.5º per dalt del normal`
                     },
                     highlight: {
                         contentClass: 'anomaly-hot--2',
@@ -71,7 +89,7 @@ export default {
                 {
                     key: 'yellow',
                     popover: {
-                        label: `A`
+                        label: `Temperatura mitja >1.5º per dalt del normal`
                     },
                     highlight: {
                         contentClass: 'anomaly-hot--1',
@@ -82,7 +100,7 @@ export default {
                 {
                     key: 'normal',
                     popover: {
-                        label: `A`
+                        label: `Temperatura mitja normal per a la data`
                     },
                     highlight: {
                         contentClass: 'normal',
@@ -100,20 +118,21 @@ export default {
                 'orange',
                 'red'
             ],
-            colorMapRange: [-5, -3.5, -2, -1, 1, 2, 3.5, 5]
+            colorMapRange: [-5, -3.5, -1.5, 1.5, 3.5, 5]
         }
     },
     async created() {
-        /* const response = await fetch('http://ec2-3-250-218-11.eu-west-1.compute.amazonaws.com/elastic/daily_temperature/_search', {
-            method: 'POST', // *GET, POST, PUT, DELETE, etc.
-            mode: 'cors', // no-cors, *cors, same-origin
-            cache: 'force-cache', // *default, no-cache, reload, force-cache, only-if-cached
-            credentials: 'same-origin', // include, *same-origin, omit
+        const url = 'http://ec2-3-250-218-11.eu-west-1.compute.amazonaws.com/elastic/daily_temperature/_search';
+        const response = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'force-cache',
+            credentials: 'same-origin',
             headers: {
                 'Content-Type': 'application/json'
             },
-            redirect: 'follow', // manual, *follow, error
-            referrerPolicy: 'origin', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
+            redirect: 'follow',
+            referrerPolicy: 'origin',
             body: JSON.stringify({
                 "aggs": {
                     "2": {
@@ -130,59 +149,84 @@ export default {
                             }
                         }
                     }
-                },
-                "size": 0,
-                "_source": {
-                    "excludes": []
-                },
-                "stored_fields": [
-                    "*"
-                ],
-                "script_fields": {},
-                "docvalue_fields": [
-                    {
-                        "field": "date",
-                        "format": "date_time"
-                    }
-                ],
-                "timeout": "30000ms"
+                }
             })
         });
-        this.dailyTemperature = await response.json(); */
+        this.weekOfYearAvg = await response.json();
+        const response2 = await fetch(url, {
+            method: 'POST',
+            mode: 'cors',
+            cache: 'force-cache',
+            credentials: 'same-origin',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            redirect: 'follow',
+            referrerPolicy: 'origin',
+            body: JSON.stringify({
+                "size":500,
+                "sort":[
+                    {
+                        "date":{
+                            "order":"asc",
+                            "unmapped_type":"boolean"
+                        }
+                    }
+                ],
+                "query":{
+                    "bool":{
+                        "must":[
+                            {
+                            "range":{
+                                "date":{
+                                    "format":"strict_date_optional_time",
+                                    "gte":"2019-12-31T23:00:00.000Z",
+                                    "lte":"now"
+                                }
+                            }
+                            }
+                        ]
+                    }
+                }
+            })
+        });
+        this.dailyTemperature = await response2.json();
+        this.calculateCurrentYearAnomalies();
     },
-    updated() {
+     updated() {
         const calendar = this.$refs.calendar;
-        calendar && calendar.move({ month: 1, year: 2020 })
-    },
-    mounted() {
-        const firstDayOfYear = new Date ('01-01-2020');
-        let currentDayProcessed = firstDayOfYear;
-        const today = new Date();
-        let dayCounter = 0;
-        while (currentDayProcessed < today) {
-            const date = new Date(currentDayProcessed);
-            const anomaly = this.calculateDayAnomaly(currentDayProcessed, dayCounter);
-            dayCounter++;
-            const dateColor = this.getDayColorFromAnomaly(anomaly);
-            const dateCategory = this.dates.find(category => category.key === dateColor);
-            dateCategory && dateCategory.dates.push(date);
-            currentDayProcessed.setDate(currentDayProcessed.getDate() + 1);
-        }
-        this.finishProcess = true;
+        calendar && calendar.focusDate(new Date());
     },
     methods: {
+        calculateCurrentYearAnomalies() {
+            const firstDayOfYear = new Date ('01-01-2020');
+            let currentDayProcessed = firstDayOfYear;
+            const today = new Date();
+            let dayCounter = 0;
+            while (currentDayProcessed < today) {
+                const date = new Date(currentDayProcessed);
+                const anomaly = this.calculateDayAnomaly(currentDayProcessed, dayCounter);
+                dayCounter++;
+                const dateColor = this.getDayColorFromAnomaly(anomaly);
+                const dateCategory = this.dates.find(category => category.key === dateColor);
+                dateCategory && dateCategory.dates.push(date);
+                currentDayProcessed.setDate(currentDayProcessed.getDate() + 1);
+            }
+            this.finishProcess = true;
+        },
         getDayColorFromAnomaly(anomaly) {
+
             if (anomaly < this.colorMapRange[0]) {
                 return this.colorPalette[0];
-            } else if (anomaly >= this.colorMapRange[1] && anomaly < this.colorMapRange[2]) {
+            } else if (anomaly >= this.colorMapRange[0] && anomaly < this.colorMapRange[1]) {
                 return this.colorPalette[1];
-            } else if (anomaly >= this.colorMapRange[2] && anomaly < this.colorMapRange[3]) {
+            } else if (anomaly >= this.colorMapRange[1] && anomaly < this.colorMapRange[2]) {
                 return this.colorPalette[2];
-            } else if (anomaly >= this.colorMapRange[3] && anomaly < this.colorMapRange[4]) {
+            } else if (anomaly >= this.colorMapRange[2] && anomaly < this.colorMapRange[3]) {
                 return this.colorPalette[3];
-            } else if (anomaly >= this.colorMapRange[4] && anomaly < this.colorMapRange[5]) {
+            } else if (anomaly >= this.colorMapRange[3] && anomaly < this.colorMapRange[4]) {
                 return this.colorPalette[4];
-            } else if (anomaly >= this.colorMapRange[5] && anomaly < this.colorMapRange[6]) {
+            } else if (anomaly >= this.colorMapRange[4] && anomaly <= this.colorMapRange[5]) {
                 return this.colorPalette[5];
             } else {
                 return this.colorPalette[6];
@@ -191,12 +235,27 @@ export default {
         calculateDayAnomaly(date, dayOfYear) {
             try {
                 const weekOfTheYear = this.getWeekNumber(date);
-                const weeOfTheYearAvgTemperature = weekOfYearAvg.aggregations['2'].buckets.find(obj => obj.key === weekOfTheYear)['1'].value;
-                const anomaly = dailyTemperature.hits.hits[dayOfYear]._source.t_avg - weeOfTheYearAvgTemperature;
+                const weekOfTheYearAvgTemperature = this.weekOfYearAvg.aggregations['2'].buckets.find(obj => obj.key === weekOfTheYear)['1'].value;
+                const anomaly = this.dailyTemperature.hits.hits[dayOfYear]._source.t_avg - weekOfTheYearAvgTemperature;
                 return anomaly;
             } catch (e) {
                 return null;
             }
+        },
+        getDayTemperatures(day) {
+            const oneDay = 24 * 60 * 60 * 1000;
+            const weekOfTheYear = this.getWeekNumber( day.date);
+            const dayOfYear = Math.round(Math.abs((day.date - new Date ('01-01-2020')) / oneDay));
+            const weekOfTheYearAvgTemperature = this.weekOfYearAvg.aggregations['2'].buckets.find(obj => obj.key === weekOfTheYear)['1'].value;
+            const dayTemperature = this.dailyTemperature.hits.hits[dayOfYear]._source.t_avg;
+            console.log(dayTemperature);
+            console.log(weekOfTheYearAvgTemperature);
+            this.daySelected = {
+                day: day.ariaLabel,
+                dayTemperature: dayTemperature,
+                normalDayTemperature: weekOfTheYearAvgTemperature
+            }
+            this.showDateInfo = true;
         },
         getWeekNumber(d) {
             // Copy date so don't modify original

03fd5a772ad99624f13fb18e07f82f9d857b8cb9
Sergi Soler <ssoler@visualnacert.com>
2020-10-15 09:39:19 +0200

    build manual

---
+++ client/page/assets/f0783f9.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{222:function(t,n,e){"use strict";e.r(n);var c={created:function(){this.$router.push("/".concat(2020))}},r=e(8),component=Object(r.a)(c,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);n.default=component.exports}}]);

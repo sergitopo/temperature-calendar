e0fb75c51918e45124150599e2f90bb13c901e75
sergitopo <sergitopodeseca@gmail.com>
2022-01-04 10:57:56 +0100

    manual build

---
+++ client/page/assets/pages/index.9111601.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{226:function(t,n,e){"use strict";e.r(n);var c=e(110),r={created:function(){this.$router.push("/".concat(c.a))}},o=e(12),component=Object(o.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div")}),[],!1,null,null,null);n.default=component.exports}}]);

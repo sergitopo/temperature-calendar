50e259cf5be783a673200d4e4f635205b8bc95de
Sergi Soler <sergi.soler@webfleet.com>
2026-01-09 13:18:37 +0100

    adapt router nuxt config

--- src/frontend/nuxt.config.js
+++ src/frontend/nuxt.config.js
@@ -2,12 +2,12 @@ export default {
     srcDir: 'src/',
     plugins: [{ src: '@/plugins/calendar', mode: 'client'}, { src: '@/plugins/menu', mode: 'client'}],
     env: {
-        baseURL: (process.env.NODE_ENV === 'production' ? 'https://sergitopo.github.io/temperature-calendar/client/page/' : 'http://localhost:3000/')
+        baseURL: (process.env.NODE_ENV === 'production' ? 'https://sergitopo.github.io/temperature-calendar' : 'http://localhost:3000/')
     },
     components: true,
     target: 'static',
     router: {
-        base: process.env.NODE_ENV === 'production' ? '/temperature-calendar/client/page/' : '/'
+        base: process.env.NODE_ENV === 'production' ? '/temperature-calendar/' : '/',
     },
     build: {
         publicPath: '/assets/'

e7b5ba0f33df42b29f8f6703abd6cfdd59fa8d52
sergitopo <sergitopo@users.noreply.github.com>
2026-01-09 13:56:24 +0000

    Deploy: Build de producció push (9e18248b93740901b7b970fd028e990220c87270) 9e18248b93740901b7b970fd028e990220c87270

---
+++ assets/static/1767966983/monthly/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Ftemperature-calendar\u002Fassets\u002Fstatic\u002F1767966983",layout:"menu",error:a,serverRendered:true,routePath:"\u002Fmonthly",config:{_app:{basePath:"\u002Ftemperature-calendar\u002F",assetsPath:"\u002Ftemperature-calendar\u002Fassets\u002F",cdnURL:a}}}}(null));
